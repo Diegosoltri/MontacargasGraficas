e0138f48b7dd5c1af4d4b22234fe2e1047f77d53
OshkarVTec <a01275287@tec.mx>
2023-08-24 10:09:54 -0600

    lifter faces direction

--- Lifter.py
+++ Lifter.py
@@ -1,4 +1,3 @@
-
 import pygame
 from pygame.locals import *
 from Cubo import Cubo
@@ -14,7 +13,6 @@ import math
 
 class Lifter:
     def __init__(self, dim, vel, textures, txtIndex):
-
         self.dim = dim
         # Se inicializa una posicion aleatoria en el tablero
         # self.Position = [random.randint(-dim, dim), 2, random.randint(-dim, dim)]
@@ -25,26 +23,28 @@ class Lifter:
         # Se inicializa un vector de direccion aleatorio
         dirX = random.randint(-10, 10) or 1
         dirZ = random.randint(-1, 1) or 1
-        magnitude = math.sqrt(dirX * dirX + dirZ * dirZ) * vel
-        self.Direction = [dirX / magnitude, 0, dirZ / magnitude]
+        magnitude = math.sqrt(dirX ** 2 + dirZ ** 2)
+        self.Direction = [(dirX / magnitude), 0, (dirZ / magnitude)]
+        self.angle = 0
+        self.vel = vel
         # El vector aleatorio debe de estar sobre el plano XZ (la altura en Y debe ser fija)
         # Se normaliza el vector de direccion
         # ...
         # Se cambia la maginitud del vector direccion con la variable vel
         # ...
-        
-        #Arreglo de texturas
+
+        # Arreglo de texturas
         self.textures = textures
 
-        #Index de la textura a utilizar
+        # Index de la textura a utilizar
         self.txtIndex = txtIndex
 
     def update(self):
         # Se debe de calcular la posible nueva posicion del cubo a partir de su
         # posicion acutual (Position) y el vector de direccion (Direction)
         # ...
-        newX = self.Position[0] + self.Direction[0]
-        newZ = self.Position[2] + self.Direction[2]
+        newX = self.Position[0] + self.Direction[0] * self.vel
+        newZ = self.Position[2] + self.Direction[2] *self.vel
         if newX < -self.dim or newX > self.dim:
             self.Direction[0] *= -1
         else:
@@ -53,6 +53,10 @@ class Lifter:
             self.Direction[2] *= -1
         else:
             self.Position[2] = newZ
+        self.angle = math.acos(self.Direction[0]) * 180 / math.pi
+        if (self.Direction[2] > 0):
+            self.angle = 360 - self.angle
+        print(self.angle)
 
         # Se debe verificar que el objeto cubo, con su nueva posible direccion
         # no se salga del plano actual (DimBoard)
@@ -61,13 +65,14 @@ class Lifter:
     def draw(self):
         glPushMatrix()
         glTranslatef(self.Position[0], self.Position[1], self.Position[2])
+        glRotatef(self.angle, 0, 1, 0)
         # Se dibuja el cubo
         # ...
         glScaled(5, 5, 5)
         glColor3f(1.0, 1.0, 1.0)
-        #glEnable(GL_TEXTURE_2D)
-        #front face
-        #glBindTexture(GL_TEXTURE_2D, self.textures[self.txtIndex])
+        # glEnable(GL_TEXTURE_2D)
+        # front face
+        # glBindTexture(GL_TEXTURE_2D, self.textures[self.txtIndex])
         glBegin(GL_QUADS)
         glTexCoord2f(0.0, 0.0)
         glVertex3d(1, 1, 1)
@@ -78,7 +83,7 @@ class Lifter:
         glTexCoord2f(1.0, 0.0)
         glVertex3d(1, -1, 1)
 
-        #2nd face
+        # 2nd face
         glTexCoord2f(0.0, 0.0)
         glVertex3d(-2, 1, 1)
         glTexCoord2f(0.0, 1.0)
@@ -88,7 +93,7 @@ class Lifter:
         glTexCoord2f(1.0, 0.0)
         glVertex3d(-2, -1, 1)
 
-        #3rd face
+        # 3rd face
         glTexCoord2f(0.0, 0.0)
         glVertex3d(-2, 1, -1)
         glTexCoord2f(0.0, 1.0)
@@ -98,7 +103,7 @@ class Lifter:
         glTexCoord2f(1.0, 0.0)
         glVertex3d(-2, -1, -1)
 
-        #4th face
+        # 4th face
         glTexCoord2f(0.0, 0.0)
         glVertex3d(1, 1, -1)
         glTexCoord2f(0.0, 1.0)
@@ -108,7 +113,7 @@ class Lifter:
         glTexCoord2f(1.0, 0.0)
         glVertex3d(1, -1, -1)
 
-        #top
+        # top
         glTexCoord2f(0.0, 0.0)
         glVertex3d(1, 1, 1)
         glTexCoord2f(0.0, 1.0)
@@ -120,50 +125,50 @@ class Lifter:
 
         glEnd()
 
-        #Wheels
+        # Wheels
         glPushMatrix()
-        glTranslatef(0,1.5,0)
-        glScaled(0.8,0.8,0.8)
+        glTranslatef(0, 1.5, 0)
+        glScaled(0.8, 0.8, 0.8)
         glColor3f(0.0, 0.0, 1.0)
         head = Cubo(self.textures, 0)
         head.draw()
         glPopMatrix()
 
         glPushMatrix()
-        glTranslatef(-1.2,-1,1)
-        glScaled(0.3,0.3,0.3)
+        glTranslatef(-1.2, -1, 1)
+        glScaled(0.3, 0.3, 0.3)
         glColor3f(0.0, 0.0, 1.0)
         wheel = Cubo(self.textures, 0)
         wheel.draw()
         glPopMatrix()
 
         glPushMatrix()
-        glTranslatef(0.5,-1,1)
-        glScaled(0.3,0.3,0.3)
+        glTranslatef(0.5, -1, 1)
+        glScaled(0.3, 0.3, 0.3)
         glColor3f(0.0, 0.0, 1.0)
         wheel = Cubo(self.textures, 0)
         wheel.draw()
         glPopMatrix()
 
         glPushMatrix()
-        glTranslatef(0.5,-1,-1)
-        glScaled(0.3,0.3,0.3)
+        glTranslatef(0.5, -1, -1)
+        glScaled(0.3, 0.3, 0.3)
         glColor3f(0.0, 0.0, 1.0)
         wheel = Cubo(self.textures, 0)
         wheel.draw()
         glPopMatrix()
 
         glPushMatrix()
-        glTranslatef(-1.2,-1,-1)
-        glScaled(0.3,0.3,0.3)
+        glTranslatef(-1.2, -1, -1)
+        glScaled(0.3, 0.3, 0.3)
         glColor3f(0.0, 0.0, 1.0)
         wheel = Cubo(self.textures, 0)
         wheel.draw()
         glPopMatrix()
 
-        #Lifter
+        # Lifter
         glPushMatrix()
-        glTranslatef(0,-1.5,0) #Up and down
+        glTranslatef(0, -1.5, 0)  # Up and down
         glBegin(GL_QUADS)
         glTexCoord2f(0.0, 0.0)
         glVertex3d(1, 1, 1)
@@ -175,6 +180,4 @@ class Lifter:
         glVertex3d(3, 1, 1)
         glEnd()
         glPopMatrix()
-
-
         glPopMatrix()
